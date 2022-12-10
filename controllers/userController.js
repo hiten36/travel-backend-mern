a42d826a9b6ab08c8b335eecf7717f1fc2584500
hiten <hitenkhatri14@gmail.com>
2022-12-11 01:35:44 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const User=require('../models/user');
 const bcrypt=require('bcryptjs');
+const auth = require('../middleware/auth');
+const { removeUndefined } = require('../util/util');
 
 const signin=async ({ firstName, lastName, email, gender, phone, age, password, confirmPassword })=>{
     let emailCheck=await User.findOne({email});
@@ -37,8 +39,29 @@ const getUsers=async ()=>{
     return {success:true, data};
 };
 
+const updateUser=async ({firstName, lastName, email, gender, phone, age})=>{
+    if(!authAdmin)
+    {
+        return {success: false, message: "Not Authorised"}
+    }
+    let updateObj=removeUndefined({firstName, lastName, email, gender, phone, age});
+    let updateProfile=await User.findByIdAndUpdate(authAdmin._id, {$set:updateObj}, {new:true});
+    return {success:true, updateProfile};
+};
+
+const deleteUser=async (id, authAdmin)=>{
+    if(!authAdmin)
+    {
+        return {success: false, message: "Not Authorised"}
+    }
+    await User.findByIdAndDelete(id);
+    return {success:true, message: "User Deleted"};
+};
+
 module.exports={
     signin,
     login,
-    getUsers
+    getUsers,
+    updateUser,
+    deleteUser
 };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,4 @@
-const { signin, login, getUsers } = require('../controllers/userController');
+const { signin, login, getUsers, updateUser, deleteUser } = require('../controllers/userController');
 const auth=require('../middleware/auth');
 const router=require('express').Router();
 
@@ -23,6 +23,24 @@ router.post('/login',async (req,res)=>{
 router.get('/getUsers',async (req,res)=>{
     try {
         const data = await getUsers();
+        res.json(data);
+    } catch (error) {
+        res.status(400).json({success:false, message:error.message});
+    }
+});
+
+router.put('/updateProfile',async (req,res)=>{
+    try {
+        const data = await updateUser({...req.body, authAdmin: req.user});
+        res.json(data);
+    } catch (error) {
+        res.status(400).json({success:false, message:error.message});
+    }
+});
+
+router.put('/deleteUser/:userId',async (req,res)=>{
+    try {
+        const data = await deleteUser(req.user, req.params.userId);
         res.json(data);
     } catch (error) {
         res.status(400).json({success:false, message:error.message});
